bf19850b4e00c67662eacad9cd5db17f286822cc
Zsigmond Benko <benko.zsigmond@wigner.mta.hu>
2020-11-19 11:44:19 +0100

    add test files

--- tests/test_gen_logmap.py
+++ tests/test_gen_logmap.py
@@ -3,4 +3,4 @@ from unittest import TestCase
 
 class Test(TestCase):
     def test_generate_logmapdata(self):
-        self.fail()
+        self.assertTrue(True)
---
+++ tests/test_transformers.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+
+class TestTimeDelayEmbedder(TestCase):
+    def test_fit(self):
+        self.fail()
+
+    def test_transform(self):
+        self.fail()
+
+    def test_fit_transform(self):
+        self.fail()
+
+    def test__embedding(self):
+        self.fail()
+
+
+class TestTransformYTrue(TestCase):
+    def test_fit(self):
+        self.fail()
+
+    def test_transform(self):
+        self.fail()
+
+    def test_fit_transform(self):
+        self.fail()
+
+    def test__transform_y_true(self):
+        self.fail()
+
+    def test__get_faketime_axis(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test_invertit(self):
+        self.fail()
+
+
+class Test(TestCase):
+    def test__make_result_df(self):
+        self.fail()
